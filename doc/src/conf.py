f8b1eeef77176dfa064efea466bed13919dc5efc
Rotzbua <Rotzbua@users.noreply.github.com>
2026-01-04 18:00:38 +0100

    fix(doc): revert sphinx & correct notfound

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -16,24 +16,24 @@ subprocess.call('doxygen Doxyfile', shell=True)
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'RFID_MFRC522v2'
-copyright = '2023, GithubCommunity'
+project_copyright = '%Y, GithubCommunity'
 author = 'GithubCommunity'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'breathe',
-    "exhale",
-    'myst_parser',
-    'notfound.extension',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
+  'breathe',
+  'exhale',
+  'myst_parser',
+  'notfound.extension',
+  'sphinx.ext.autodoc',
+  'sphinx.ext.coverage',
+  'sphinx.ext.githubpages',
+  'sphinx.ext.ifconfig',
+  'sphinx.ext.inheritance_diagram',
+  'sphinx.ext.todo',
+  'sphinx.ext.viewcode',
 ]
 
 templates_path = ['_templates']
@@ -43,9 +43,9 @@ exclude_patterns = ['Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'literal',
-    '.md': 'markdown',
+  '.rst': 'restructuredtext',
+  '.txt': 'literal',
+  '.md': 'markdown',
 }
 
 # If this is not None, a ‘Last updated on:’ timestamp is inserted at every page bottom.
@@ -54,33 +54,33 @@ html_last_updated_fmt = ''
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
 html_theme = 'nature'
 html_static_path = ['_static']
 
 ### Custom plugins ###
 ## Markdown
 myst_enable_extensions = [
-    "linkify",
-    "smartquotes",
-    "strikethrough",
-    "tasklist",
+  'linkify',
+  'smartquotes',
+  'strikethrough',
+  'tasklist',
 ]
 ## Bridge doxygen <> sphinx.
 breathe_projects = {
-    'MFRC522': "./_doxygen/xml/"
+  'MFRC522': './_doxygen/xml/'
 }
 breathe_default_project = 'MFRC522'
 breathe_default_members = (
-    'members', 
-    # 'undoc-members',
+  'members',
+  # 'undoc-members',
 )
-## Auto generate doc for every cpp-file. 
+## Auto generate doc for every cpp-file.
 exhale_args = {
-    "containmentFolder": "./lib",
-    "doxygenStripFromPath": "..",
-    "rootFileName": "index.rst",
-    "rootFileTitle": "Library API",
-    # "createTreeView":        True,
+  'containmentFolder': './lib',
+  'doxygenStripFromPath': '..',
+  'rootFileName': 'index.rst',
+  'rootFileTitle': 'Library API',
+  # 'createTreeView':        True,
 }
-
+## notfound
+notfound_urls_prefix = '/Arduino_MFRC522v2/'
